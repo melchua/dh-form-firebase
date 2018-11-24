337f19b359913f25f34bb8bb49a13892fbf7b910
Mel <mchuam@gmail.com>
2018-11-23 17:14:12 -0800

    completed

--- app.js
+++ app.js
@@ -14,17 +14,39 @@
 
   const docRef = firestore.doc("test/relationData");
 
+  // linking to dom elements
+  const seriesList = document.querySelector('#seriesList');
   const form = document.querySelector("#addSeriesForm")
 
   // render the data
-  function renderData(doc) {
+  function renderSeries(doc) {
+    let li = document.createElement('li');
     let seriesName = document.createElement('span');
     let instructorName = document.createElement('span');
     let seriesPoster = document.createElement('span');
+    let cross = document.createElement('div');
 
+    li.setAttribute('data-id', doc.id);
+    li.setAttribute('class', 'style: list-group-item')
     seriesName.textContent = doc.data().seriesName;
     instructorName.textContent = doc.data().instructorName;
     seriesPoster.textContent = doc.data().seriesPoster;
+    cross.textContent = 'x';
+
+    li.appendChild(seriesName);
+    li.appendChild(instructorName);
+    li.appendChild(seriesPoster);
+    li.appendChild(cross);
+    seriesList.appendChild(li);    
+
+    console.log(li.getAttribute('data-id'));
+
+    // deleting data
+    cross.addEventListener('click', (e) => {
+      e.stopPropagation();
+      let id = e.target.parentElement.getAttribute('data-id');
+      firestore.collection('series').doc(id).delete();
+    });
   }
 
   // saving data
@@ -33,13 +55,32 @@
     firestore.collection('series').add({
       seriesName: form.seriesName.value,
       instructorName: form.instructorName.value,
-      seriesPoster: form.seriesPoster.value
-    })
+      seriesPoster: form.seriesPoster.value,
+    });
+
+    form.seriesName.value = "";
+    form.instructorName.value = "";
+    form.seriesPoster.value = "";
   });  
 
   // getting data
-  firestore.collection('series').get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
-      console.log(doc.data());
+  // firestore.collection('series').orderBy('seriesName').get().then((snapshot) => {
+  //   snapshot.docs.forEach(doc => {
+  //     renderSeries(doc);
+  //   });
+  // })
+
+  // realtime listener
+
+  firestore.collection('series').orderBy('seriesName').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+      // console.log(change.doc.data());
+      if (change.type == "added") {
+        renderSeries(change.doc);
+      } else if (change.type == "removed") {
+          let li = seriesList.querySelector('[data-id=' + change.doc.id + ']');
+          seriesList.removeChild(li);
+      }
     });
   })
